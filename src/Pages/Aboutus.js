6725864f12b734385a4b2ebe27ab39118b87ff54
jiikeeesh <78836181+jiikeeesh@users.noreply.github.com>
2022-06-08 12:08:37 +0545

    about

--- src/Pages/Aboutus.js
+++ src/Pages/Aboutus.js
@@ -5,7 +5,69 @@ import MainNavbar from './MainNavbar'
 const Aboutus = () => {
   return (
     <>
-    <h1>hello</h1>
+    <MainNavbar/>
+    <p>
+        Why Study in India?
+Why is India a great destination to study as an international student
+India&#39;s diversity and history offer a wide array of options for any student. India&#39;s
+educational system is as vast and diverse as the country itself. While living in India can
+be challenging for international students, it is an affordable and rewarding experience.
+A millennia-old civilization, the birthplace of four world religions, a dazzling diversity of
+cultures, languages, and traditions, and breathtaking natural beauty – from the soaring
+Himalayas to the deserts of Rajasthan. The real question is not “why study in India?”,
+but “why wouldn&#39;t you?” While India has much to offer the traveler, it also has much to
+offer students. With access both to such rich history and diversity as well as a rapidly
+growing country on the cutting edge of the technological revolution, studying in India
+has much to offer, whether your interests are in Classics or Computers, Philosophy or
+Politics.
+India&#39;s educational system is as encompassing and as diverse as its history, making
+studying in India unlike anywhere else. India is the home of the world&#39;s largest university
+by enrollment, Indira Gandhi National Open University, with 3.5 million students. India&#39;s
+higher education system has 575 university-level institutions, including:
+ 42 central universities, which are controlled by National Department of Higher
+Education.
+ 243 state universities, which are run by state governments
+ 130 deemed universities, which are autonomously run
+ 53 private universities
+ 33 institutes of national importance, which are deemed to be of special
+importance and typically focus on science and technology
+Additionally, over 30,000 colleges operate throughout India, offering more specialized
+instruction, often in technical fields like engineering. With all these options, international
+students in India can find a program to suit them no matter what their interests.
+India&#39;s degree system is influenced by the British higher education and offers
+bachelor&#39;s, master&#39;s, and doctoral degrees.
+The Indian Government&#39;s Department of Higher Education offers more information on
+specific universities in India and the higher education system.
+
+Virtual Learning – meaning
+
+Virtual learning is a learning experience that is enhanced through utilizing computers and/or the
+internet both outside and inside the facilities of the educational organization. The instruction
+most commonly takes place in an online environment. The teaching activities are carried out
+online whereby the teacher and learners are physically separated (in terms of place, time, or
+both).
+
+WORK WHILE YOU STUDY
+Now if you working somewhere and you have to complete your studies along
+with your job.
+So, you have India&#39;s Best Universities which provide, Virtual Classes that is Any
+Time Classes which provides you the amazing Movie Like CLASS which you can
+take anywhere without leaving Your job.
+
+Working while studying also has advantages
+For example, working enables the development of skills that&#39;ll look great on your CV -
+especially if your job is related to your study area. At work, you&#39;ll develop skills such as
+organisation and time management, especially as you&#39;ll be prioritising multiple workloads
+ Earning extra money.
+ Seeking networking opportunities.
+ Gaining professional experience.
+ Developing transferable skills.
+ Highlighting your ambition.
+ Learning money management.
+ Improving your grades.
+ Applying classroom learning and receiving paid training.
+    </p>
+    <Footer/>
     </>
   )
 }
